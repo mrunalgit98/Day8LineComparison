334fcab1f52fc01dd231705e8c892a33e9d4064a
mrunal <mrunalbhoir100@gmail.com>
2022-07-16 11:59:06 +0530

    UC2_LINE EQUALITY

--- src/com/linecomparison/lineComparison.java
+++ src/com/linecomparison/lineComparison.java
@@ -6,29 +6,65 @@ public class lineComparison {
 
 	static double length(int x1,int y1 ,int x2,int y2)
 	{
-		double value =Math.pow(x2, x1)+Math.pow(y2, y1);
+		double value =((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1));
+		double length=Math.sqrt(value);
 		return value;
 	}
-	public static void main(String[] args) {
-		System.out.println("ENTER FIRST COORDINATE X");
+	
+	 public static void lineEquality(double l1,double l2){
+	        
+	        if(l1==l2)
+	        {
+	            System.out.println("Both the lines are equal");
+	        }
+	        else
+	        {
+	            System.out.println("Both the lines are not equal");
+	        }
+	 }
+        public static void main(String[] args) {
+		System.out.println("ENTER FIRST COORDINATE X1");
 		Scanner x1=new Scanner(System.in);
 		int a1=x1.nextInt();
 		
-		System.out.println("ENTER SECOND COORDINATE Y");
+		System.out.println("ENTER SECOND COORDINATE Y1");
 		Scanner x2=new Scanner(System.in);
 		int a2=x2.nextInt();
 		
-		System.out.println("ENTER FIRST COORDINATE X");
+		System.out.println("ENTER FIRST COORDINATE X2");
 		Scanner y1=new Scanner(System.in);
         int b1=y1.nextInt();
 		
 		
-		System.out.println("ENTER SECOND COORDIANTE Y");
+		System.out.println("ENTER SECOND COORDIANTE Y2");
 		Scanner y2=new Scanner(System.in);
         int b2=y2.nextInt();
         
-       double value=length(a1,a2,b1,b2);
+       double l1=length(a1,a2,b1,b2);
+       System.out.println("length of line1 is "+l1 );
        
-       System.out.println("length of line is "+Math.sqrt(value)  );
+       
+       
+       System.out.println("ENTER FIRST COORDINATE X3");
+		Scanner x3=new Scanner(System.in);
+		int a3=x3.nextInt();
+		
+		System.out.println("ENTER SECOND COORDINATE Y3");
+		Scanner x4=new Scanner(System.in);
+		int a4=x4.nextInt();
+		
+		System.out.println("ENTER SECOND COORDINATE Y3");
+		Scanner y3=new Scanner(System.in);
+		int b3=y3.nextInt();
+		
+		System.out.println("ENTER SECOND COORDINATE Y3");
+		Scanner y4=new Scanner(System.in);
+		int b4=y4.nextInt();
+		
+		double l2=length(a3,a4,b3,b4);
+	     System.out.println("length of line1 is "+l2  );
+	     
+	     lineComparison.lineEquality(l1, l2);
+       
 	}
 }
